2b409e2a992901b09a99289e3f56aba1cb59a935
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2024-01-19 19:44:55 +0100

    Delete tt directory

--- tt/main.py
+++
@@ -1,8 +0,0 @@
-
-def main ():
-	print("hello from python");
-	print("hello from python");
-
-__name__ = '__main__';
-if __name__ == '__main__':main();
-#print (__name__);
--- tt/my_age.py
+++
@@ -1,9 +0,0 @@
-import datetime
-def main ():
-	DOB=input("Enter your Year Birth: ")
-	YearNow=datetime.datetime.now().year
-	MyAge=YearNow-int(DOB)
-	print("Your age is {} Year".format(MyAge))
-
-
-if __name__ == '__main__':main();
